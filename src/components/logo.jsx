4073cecb924d97e3002365ce665243131ea88d42
James Pike <jamesandrewpike@gmail.com>
2024-11-04 13:09:43 -0500

    commit

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -3,7 +3,7 @@
 import { component$ } from "@builder.io/qwik";
 
 // @ts-ignore
-import logoSrc from "/images/logo.png?width=120&height=40&png";
+import logoSrc from "/images/logo.png";
 
 export default component$(() => (
   <span class="self-center ml-2 text-2xl md:text-xl font-bold text-gray-900 whitespace-nowrap dark:text-white flex items-center">
