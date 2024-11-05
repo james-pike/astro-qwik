fe941ea3da6fab1c981dc717740f64eec2f16eba
jvm3 <jamesandrewpike@gmail.com>
2024-11-04 22:23:15 -0500

    commit

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -111,10 +111,10 @@ const Layout = component$<Props>(({ fullWidth, slug, search }) => {
           onClick$={handleNav}
         ></div>
 
-        <div class='max-w-8xl pt-[72px] mx-auto px-4 sm:px-6 md:px-8'>
+        <div class='max-w-8xl pt-[72px] mx-auto px-4 sm:px-6 md:px-8 '>
           <div
             id='sidebar'
-            class={`lg:block fixed inset-0 top-[73px] transition-all duration-300 right-auto w-[14.5rem] py-4 px-6 overflow-y-auto border-r border-brand-outline bg-brand-background lg:left-[max(0px,calc(50%-45rem))] z-20 ${
+            class={`lg:block fixed inset-0 top-[73px] transition-all duration-300 right-auto w-[14.5rem] bg-gradient-to-r from-blue-100 to-slate-200 py-4 px-6 overflow-y-auto border-r border-brand-outline bg-brand-background lg:left-[max(0px,calc(50%-45rem))] z-20 ${
               isNavOpen.value ? 'left-[max(0px,calc(50%-45rem))]' : '-left-[240px]'
             }`}
           >
